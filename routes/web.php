35bfe1fc60cedbd6da1cff361577de0fedfed40f
Dr-Lab1 <jbkukwabantu@gmail.com>
2026-05-21 23:44:23 +0100

    Fixing global CSS

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('posts.index');
+    return view('dashboard.comments');
 });
